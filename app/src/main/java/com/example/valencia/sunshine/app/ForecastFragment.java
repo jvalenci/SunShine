44f4b026b8fe2a01583867895137bcd3ca20757b
Jonathan Valencia <jonathan.valencia716@gmail.com>
2014-12-27 10:35:06 -0500

    add detailActivity for each item in the list, as well as the list gets updated with weather data

--- app/src/main/java/com/example/valencia/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/valencia/sunshine/app/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.valencia.sunshine.app;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -11,8 +12,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -57,7 +60,7 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if(id == R.id.action_refresh){
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("33617");
+            weatherTask.execute("33617, USA");
 
             return true;
         }
@@ -92,6 +95,17 @@ public class ForecastFragment extends Fragment {
 
         ListView listView = (ListView)rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
+
+        //this is the code to set the toast
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String forecast = mForecastAdapter.getItem(position);
+                Intent intent = new Intent(getActivity(),DetailActivity.class).putExtra(Intent.EXTRA_TEXT, forecast);
+                startActivity(intent);
+            }
+        });
+
 
 
 
